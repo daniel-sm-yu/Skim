b8e4674a7a6eae4a3e12a5710b9319a002fc7fb8
Daniel Yu <daniel.sm.yu@gmail.com>
2019-03-19 22:05:16 -0400

    initial commit

---
+++ app/src/main/java/com/dsyu/skim/MainActivity.java
@@ -0,0 +1,44 @@
+package com.dsyu.skim;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_back:
+                    mTextMessage.setText( R.string.title_back );
+                    return true;
+                case R.id.navigation_read:
+                    mTextMessage.setText( R.string.title_read );
+                    return true;
+                case R.id.navigation_next:
+                    mTextMessage.setText( R.string.title_next );
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        mTextMessage = (TextView) findViewById( R.id.message );
+        BottomNavigationView navigation = (BottomNavigationView) findViewById( R.id.navigation );
+        navigation.setOnNavigationItemSelectedListener( mOnNavigationItemSelectedListener );
+    }
+
+}
