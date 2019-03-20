60e5c1eb37a40b46ede1240752d5245437b7b588
Daniel Yu <daniel.sm.yu@gmail.com>
2019-03-20 17:25:48 -0400

    connected to API using OkHttp

---
+++ app/src/main/java/com/dsyu/skim/CurrentNews.java
@@ -0,0 +1,4 @@
+package com.dsyu.skim;
+
+public class CurrentNews {
+}
--- app/src/main/java/com/dsyu/skim/MainActivity.java
+++ app/src/main/java/com/dsyu/skim/MainActivity.java
@@ -4,11 +4,25 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -39,6 +53,42 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById( R.id.message );
         BottomNavigationView navigation = (BottomNavigationView) findViewById( R.id.navigation );
         navigation.setOnNavigationItemSelectedListener( mOnNavigationItemSelectedListener );
+
+        getNews( "fifa" );
+    }
+
+    private void getNews(String topic) {
+
+        String apiKEY = BuildConfig.skim_key;
+
+        String newsURL = "https://newsapi.org/v2/everything?apiKey=" + apiKEY + "&q=" + topic;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(newsURL).build();
+
+        Call call = client.newCall( request );
+
+        call.enqueue( new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.e( TAG, response.body().string() );
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } );
+
+
+
     }
 
 }
